ab03a7de27fee33fbce6fb876455f7b743e6c9c4
venkat <venkat@local>
2023-08-16 16:11:46 -0400

    spring cloud config loading props from jdbc

--- springboot-aws-sqs/src/main/java/org/example/aws/sqs/SpringBootAwsSqsApplication.java
+++ springboot-aws-sqs/src/main/java/org/example/aws/sqs/SpringBootAwsSqsApplication.java
@@ -1,14 +1,25 @@
 package org.example.aws.sqs;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication(exclude = {ContextStackAutoConfiguration.class})
 @Slf4j
 public class SpringBootAwsSqsApplication {
+    @Value("${test-property}")
+    private String testProp;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAwsSqsApplication.class, args);
+    }
+
+    @PostConstruct
+    public void testMethod() {
+        log.info("loading property from DB ---> " + testProp);
     }
 }
